5db199925edc406daacf726fea4a63f45e52648d
rdudley123 <rad1@soton.ac.uk>
2014-02-05 16:53:57 +0000

    Update map.js

--- map/js/map.js
+++ map/js/map.js
@@ -98,9 +98,7 @@ var country = svg.selectAll(".country").data(countries);
     // Respond to mouse actions
     country
       .on("mousemove", function(d,i) {
-      	d3.select(this).style("stroke","magenta").style("stroke-width","1.2px");
-      	d3.select(this).transition().attr("x",320);
-      	d3.select(this).transition().attr("x",-320);
+      	d3.select(this).style("stroke","magenta").style("stroke-width","1.2px").transition().attr("x",320);
       	$('#info').html("<h2>" + d.name + " </h2>Population: <span class='population'>" + 
       	d.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +"</span>");
       })
